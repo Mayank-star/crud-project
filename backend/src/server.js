63f69c626ee310459977425590dbd1b91d7fcf49
Mayank-kumar-jivocare <mayank.kumar@jivocare.com>
2026-01-06 12:53:41 +0530

    connect F & E , fix mysql connection error

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,16 @@
 import dotenv from "dotenv";
-dotenv.config();
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({
+  path: path.resolve(__dirname, "../.env"),
+});
 
 import app from "./app.js";
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+app.listen(process.env.PORT, () => {
+  console.log("DB USER:", process.env.DB_USER);
+  console.log(`Server running on port ${process.env.PORT}`);
+});
